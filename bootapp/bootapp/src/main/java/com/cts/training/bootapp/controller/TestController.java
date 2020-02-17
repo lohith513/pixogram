fc83473456e4141373e37f8e7f6998b32d5da3ac
lohith513 <36068611+lohith513@users.noreply.github.com>
2020-02-17 04:19:04 -0800

    Add files via upload

---
+++ bootapp/bootapp/src/main/java/com/cts/training/bootapp/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.cts.training.bootapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+
+
+public class TestController {
+
+}
